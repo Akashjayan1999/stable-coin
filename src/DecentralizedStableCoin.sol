2a95cba5f16e78f73fadd8e90b13ed1ec8011c14
Akashjayan1999 <akashjayan999@gmail.com>
2025-04-06 09:50:34 +0000

    feat: implement DSCEngine contract with collateral management and minting logic

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -24,8 +24,8 @@
 // view & pure functions
 pragma solidity ^0.8.12;
 
-import { ERC20Burnable, ERC20 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 /**
  * @title DecentralizedStableCoin
  * @author Akash Jayan
@@ -34,29 +34,30 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
  * Value (Relative Stability): Anchored (Pegged to USD)
  * Collateral Type: Crypto
  *
-* This is the contract meant to be owned by DSCEngine. It is a ERC20 token that can be minted and burned by the
-DSCEngine smart contract.
+ * This is the contract meant to be owned by DSCEngine. It is a ERC20 token that can be minted and burned by the
+ * DSCEngine smart contract.
  */
 
-contract DecentralizedStableCoin is ERC20Burnable,Ownable {
-  error DecentralizedStableCoin__AmountMustBeMoreThanZero();
-  error DecentralizedStableCoin__BurnAmountExceedsBalance();
-  error DecentralizedStableCoin__NotZeroAddress();
-  constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266) {}
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__AmountMustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
 
-  function burn(uint256 _amount) public override onlyOwner {
-    uint256 balance = balanceOf(msg.sender);
-    if(_amount <=0){
-        revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
-    }
-    if (balance < _amount) {
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266) {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
+        }
+        if (balance < _amount) {
             revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
     }
-   super.burn(_amount);
-  }
 
-  function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
-    if (_to == address(0)) {
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
             revert DecentralizedStableCoin__NotZeroAddress();
         }
         if (_amount <= 0) {
@@ -64,5 +65,5 @@ contract DecentralizedStableCoin is ERC20Burnable,Ownable {
         }
         _mint(_to, _amount);
         return true;
-  }
+    }
 }
